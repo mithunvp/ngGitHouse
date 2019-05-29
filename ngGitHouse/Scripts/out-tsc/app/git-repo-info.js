4a5ec4355f320edbac7da434c5a8a3784a43d149
mithun.pattankar <mithun.pattankar@gmail.com>
2019-05-29 15:47:16 +0530

    Upgrade to Angular 8

--- ngGitHouse/Scripts/out-tsc/app/git-repo-info.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GitRepoInfo = /** @class */ (function () {
-    function GitRepoInfo() {
-    }
-    return GitRepoInfo;
-}());
-exports.GitRepoInfo = GitRepoInfo;
-//# sourceMappingURL=git-repo-info.js.map
